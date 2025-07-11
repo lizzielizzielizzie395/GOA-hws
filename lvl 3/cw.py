78efbc4c52e293a98f58905651f97fd4144fdbb0
lizzielizzielizzie395 <berulavamancho74@gmail.com>
2025-07-11 18:46:15 +0400

    lvl 3

---
+++ lvl 3/cw.py
@@ -0,0 +1,41 @@
+Age=15
+Age=16
+print(Age) #updated Age from 15 to 16
+
+Favsubject = "History"      #these are case-sensetive variables
+FavSubject = "chemistry"
+favSubject = "geography"
+FAVsubject = "math"
+FAVSUBJECT = "physics"
+
+#string type
+first_name = "Lizi"
+Last_name = "Gugunishvili"
+Fav_color = "pink"
+Fav_country = "Georgi"
+
+#integer type
+My_age = 140
+num_of_siblings = 2
+bank_account_balance = 10
+seat_num = 7
+
+#float type
+monthly_income = 3050.4
+temperature_in_Tbilisi = 37.6
+average_score = 87.7
+pi_approximation = 3.14
+
+print(first_name)
+print(Last_name)
+print(Fav_color)
+print(Fav_country)
+print(My_age)        
+print(num_of_siblings)
+print(num_of_siblings)    
+print(bank_account_balance)
+print(seat_num)
+print(monthly_income) 
+print(temperature_in_Tbilisi)
+print(average_score)
+print(pi_approximation)                         
---
+++ lvl 3/hw.py
@@ -0,0 +1,46 @@
+num_1 = 40
+num_2 = 10
+sum_of_num = num_1 + num_2
+difference_of_num = num_1 - num_2
+product_of_num = num_1 * num_2
+quotient_of_num = num_1 / num_2
+
+name = "Lizi"
+name = "Mari"
+name = "Nino"
+name = "Elene"
+name = "Ana"
+print(name)
+
+Fav_city = "tbilisi"
+Fav_City = "batumi"
+fav_city = 'kutaisi'
+FAV_city = 'telavi'
+FAV_CITY = 'martvili'
+print(Fav_city)
+print(Fav_City)
+print(fav_city)
+print(FAV_city)
+print(FAV_CITY)    
+
+#2name = "giorgi"  --->variable mustn't start with a number.
+name2 = "giorgi"
+#user{name = "bubunauri" --->variable must be seperated with _ or written together
+username = "bubunauri"
+#user_name = goga  --->In this case goga isnt an integer type or float type, its a string type, so we need ''/""
+user_name = "goga"
+#user-surname = axalaia --->variable must be seperated with _ or written together and axalaia written with ''/
+user_name = "axalaia"
+
+Fav_color = "pink"
+the_verb = " is"
+the_adj = " my"                  # } the sum of strings = concatenation(კონკატინაცია)
+color_adj = " favorite"
+the_noun = " color."
+the_sentence = Fav_color + the_verb + the_adj + color_adj + the_noun 
+print(the_sentence)  
+
+Name = 'lizi ' 
+Num = 10 
+my_name = Name * Num 
+print(my_name)
