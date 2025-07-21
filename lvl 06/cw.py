383fdbceb931af9ce1f585791fd37c21ff9f5b68
lizzielizzielizzie395 <berulavamancho74@gmail.com>
2025-07-21 20:04:29 +0400

    commit

---
+++ lvl 06/cw.py
@@ -0,0 +1,17 @@
+#1 - Logical operators let machines and computers evaluate complex scenarios,which is why we use them. 
+# If we have True and False-->False. but if we have True or False -->True
+
+#2
+Name_1 = input("Whats your name?: ")
+Name_2 = "Lizi"
+print(Name_1 == Name_2)
+
+#3
+situation_1 = True
+situation_2 = False
+situation_3 = True
+situation_4 = False 
+print(situation_1 or situation_2)
+print(situation_1 and situation_2)
+print(situation_2 or situation_4)
+print(situation_1 and situation_3)
---
+++ lvl 06/hw.py
@@ -0,0 +1,59 @@
+#1 
+age = int(input("How old are you?: "))
+height = int(input("How tall are you?: "))
+print(age>height)
+print(age<height)
+print(age==height)
+
+#2
+num_1 = int("10")
+num_2 = int("20")
+num_3 = int("30")
+num_4 = 40
+num_5 = 50
+multiplication = num_1*num_2*num_3*num_4*num_5
+print(multiplication)
+print(multiplication/5)
+
+#3
+first_name = input("What's your first name?: ")
+last_name = input("What's your last name?: ")
+color = input("Whats your favorite color?: ")
+Num = int(input("Whats the first number that comes to your mind?: "))
+concatination = first_name + last_name + color
+print(concatination*Num)
+
+#4 --- ჩვენ ვისწავლეთ "and" და  "or" ლოგიკური ოპერატორები. "and" უპირატესობას ანიჭებს False-ს, თუ მოცემული პირობებიდან 
+# ერთ-ერთი მაინც არ არის სწორი/შესრულებული. "or" კი უპირატესობას ანიჭებს True-ს, თუ მოცემული პირობებიდან ერთ-ერთი მაინც
+# არის სწორი/შესრულებული
+
+#5       (and)                             (or)
+# True and True -- True      |   True or True -- True           
+# True and False -- False    |   True or False -- True
+# False and True -- False    |   False or True -- True
+# False and False --False    |   False or False -- False 
+
+#6
+scenario_1 = True
+scenario_2 = False
+scenario_3 = True
+scenario_4 = False 
+print(scenario_1 or scenario_2)
+print(scenario_3 and scenario_4)
+print(scenario_2 or scenario_4)
+print(scenario_1 and scenario_3)
+
+#7
+Name = "Lizi"
+score = 100
+temperature = 34.5
+situation_1 = True 
+print(type(Name))
+print(type(score))
+print(type(temperature))
+print(type(situation_1))
+
+#8
+print(input("Whats your name?: "))
+print(int(input("How many bottles of water have u drunk today?: ")))
+print(bool(input("What's the temperature outside?: ")))
