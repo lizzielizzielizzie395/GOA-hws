a37a2a4db9d9bf161106294b96da7c845f8ab7b8
lizzielizzielizzie395 <berulavamancho74@gmail.com>
2025-09-12 20:07:52 +0400

    commit

---
+++ lvl 20/cw.py
@@ -0,0 +1,24 @@
+#1) we learned:
+
+# // - normal division, except u get integer. 
+print( 10//5) 
+print( 20//4)
+print(16//2)
+
+# ** - exponentiation
+print( 2**3) 
+print(3**3)
+print(4**2)
+
+# % - the remainder after division
+print ( 10%6) 
+print( 30% 20)
+print(40%20)
+
+
+#2) lists - let u store multiple data types and u must store the list itself in a variable, u must also use []
+#   variable - lets u store any data type 
+
+#3)
+user_info = ["Lizi", 10, 3.14159 , True]
+print(user_info)
