3c2d64c71461441316ae37d909ca818de1f8e8ac
lizzielizzielizzie395 <berulavamancho74@gmail.com>
2025-09-15 02:22:20 +0400

    commit

---
+++ lvl 22/cw.py
@@ -0,0 +1,42 @@
+# #1)
+# names = ['lizi', 'mari', 'elene','nino', 'barbare', 'qeti', 'nuca', 'jumberi', 'jemali', 'jambuli']
+
+# # difficult way
+# name_1 = names[0] 
+# name_2 = names[1]
+# name_3 = names[2]
+# name_4 = names[3]
+# name_5 = names[4]
+# name_6 = names[5]
+# name_7 = names[6]
+# name_8 = names[7]
+# name_9 = names[8]
+# name_10 = names[9]
+
+# print(name_1)
+# print(name_2)
+# print(name_3)
+# print(name_4)
+# print(name_5)
+# print(name_6)
+# print(name_7)
+# print(name_8)
+# print(name_9)
+# print(name_10)
+
+# # easy ways
+# i = 0
+
+# while i < 10:   
+#     print(names[i])
+#     i += 1
+
+# for i in range(10):
+#     print(names[i])
+
+#2)
+veggies = ['tomato', 'potato', 'cucumber','carrot','onion', 'broccoli']
+veggies[0]= 'viri'
+veggies[3]='tagvi'
+veggies[5]='bokveri'
+print(veggies)
